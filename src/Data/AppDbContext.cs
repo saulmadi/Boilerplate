ebf9f0ab08c6ddb9da5df219142c18578b15beb9
Saul Mayorquin <saulmadi@gmail.com>
2016-11-28 02:56:56 -0600

    adding entity framework model bindg

---
+++ src/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+using Domain.Entities;
+
+namespace Data
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public AppDbContext(string connectionString) :base (connectionString)
+        {
+            
+        }
+        public DbSet<Test> Tests { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Data/IAppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using Domain.Entities;
+
+namespace Data
+{
+    public interface IAppDbContext
+    {
+        DbSet<Test> Tests { get; set; }
+    }
+}
